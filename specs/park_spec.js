c3c112b72330bb2db0a6a26c7d8cfa6871e52f52
Debbie <debiurquhart@live.co.uk>
2019-12-10 19:47:15 +0000

    adds getter tests

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -1,31 +1,86 @@
-const assert = require('assert');
-const Park = require('../models/park.js');
-const Dinosaur = require('../models/dinosaur.js');
+const assert = require("assert");
+const Park = require("../models/park.js");
+const Dinosaur = require("../models/dinosaur.js");
 
-describe('Park', function() {
+// A name
+// A ticket price
+// A collection of dinosaurs
 
-  beforeEach(function () {
+describe("Park", function() {
+  beforeEach(function() {
+    park = new Park("Jurassic Park", 5, []);
+  });
 
-  })
+  it("should have a name", function() {
+    // act
+    actual = park.name;
+    // assert
+    // assert.equal(true, false);
+    assert.strictEqual(actual, expected);
+  });
 
-  it('should have a name');
+  it("should have a ticket price", function() {
+    // act
+    actual = park.ticketPrice;
+    // assert
+    // assert.equal(true, false);
+    assert.strictEqual(actual, expected);
+  });
 
-  it('should have a ticket price');
+  it("should have a collection of dinosaurs", function() {
+    // act
+    actual = park.dinosaurs;
+    // assert
+    // assert.equal(true, false);
+    assert.strictEqual(actual, expected);
+  });
 
-  it('should have a collection of dinosaurs');
+  it("should be able to add a dinosaur to its collection", function() {
+    // act
+    // assert
+    // assert.equal(true, false);
+    assert.strictEqual(actual, expected);
+  });
 
-  it('should be able to add a dinosaur to its collection');
+  it("should be able to remove a dinosaur from its collection", function() {
+    // act
+    // assert
+    // assert.equal(true, false);
+    assert.strictEqual(actual, expected);
+  });
 
-  it('should be able to remove a dinosaur from its collection');
+  it("should be able to find the dinosaur that attracts the most visitors", function() {
+    // act
+    // assert
+    // assert.equal(true, false);
+    assert.strictEqual(actual, expected);
+  });
 
-  it('should be able to find the dinosaur that attracts the most visitors');
+  it("should be able to find all dinosaurs of a particular species", function() {
+    // act
+    // assert
+    // assert.equal(true, false);
+    assert.strictEqual(actual, expected);
+  });
 
-  it('should be able to find all dinosaurs of a particular species');
+  it("should be able to calculate the total number of visitors per day", function() {
+    // act
+    // assert
+    // assert.equal(true, false);
+    assert.strictEqual(actual, expected);
+  });
 
-  it('should be able to calculate the total number of visitors per day');
+  it("should be able to calculate the total number of visitors per year", function() {
+    // act
+    // assert
+    // assert.equal(true, false);
+    assert.strictEqual(actual, expected);
+  });
 
-  it('should be able to calculate the total number of visitors per year');
-
-  it('should be able to calculate total revenue for one year');
-
+  it("should be able to calculate total revenue for one year", function() {
+    // act
+    // assert
+    // assert.equal(true, false);
+    assert.strictEqual(actual, expected);
+  });
 });
